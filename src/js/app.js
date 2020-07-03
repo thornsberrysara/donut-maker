0f6389bed7d868f41e373cfd00e4868093b4a441
Sara Thornsberry <sarathornsberry@saras-mbp.home>
2020-07-02 23:35:55 -0400

    requirements are finished

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,37 @@
+const bakeButton = document.querySelector(".click-handler__bake-button");
 const donutDisplay = document.querySelector(".click-handler__donut-display");
 const autoClicksDisplay = document.querySelector(".auto-clicker__auto-clicks-display");
+const autoClickPurchaseButton = document.querySelector(".auto-clicker__purchase-button");
+const multiplierDisplay = document.querySelector(".click-multiplier__multiplier-display");
+const multiplierPurchaseButton = document.querySelector(".click-multiplier__purchase-button");
+const resetButton = document.querySelector(".reset-button");
 const donutMaker = new DonutMaker();
 
-const bakeButton = document.querySelector(".click-handler__bake-button");
 bakeButton.addEventListener("click", () => {
     event.preventDefault();
     donutMaker.increaseDonutsAfterClick();
     donutMaker.updateDonutDisplay();
 })
 
-const purchaseButton = document.querySelector(".auto-clicker__purchase-button");
-purchaseButton.addEventListener("click", () => {
+autoClickPurchaseButton.addEventListener("click", () => {
     event.preventDefault();
-    donutMaker.increaseAutoClickerCount();
-    donutMaker.subtractDonutsAfterClickerPurchase();
-    donutMaker.updateAutoClicksDisplay();
-    donutMaker.updateDonutDisplay();
+    donutMaker.purchaseAutoClicker();
 });
+
+multiplierPurchaseButton.addEventListener("click", () => {
+    event.preventDefault();
+    donutMaker.purchaseMultiplier();
+});
+
+resetButton.addEventListener("click", () => {
+    event.preventDefault();
+    donutMaker.reset();
+})
+
+function autoClickerClicks() {
+    for (let i = 0; i < donutMaker.autoClickerCount; i++) {
+        donutMaker.increaseDonutsAfterClick();
+    }
+}
+
+setInterval(autoClickerClicks, 1000);
--- src/js/donutMaker.js
+++ src/js/donutMaker.js
@@ -2,41 +2,103 @@ class DonutMaker {
 
     constructor() {
         this.donutCount = 0;
-        this.donutDisplay = donutDisplay;
         this.autoClickerCount = 0;
-        this.autoClicksDisplay = autoClicksDisplay;
+        this.multiplierCount = 1;
         this.autoClickerCost = 100;
+        this.multiplierCost = 10;
+        this.donutDisplay = donutDisplay;
+        this.autoClicksDisplay = autoClicksDisplay;
+        this.autoClickPurchaseButton = autoClickPurchaseButton;
+        this.multiplierDisplay = multiplierDisplay;
+        this.multiplierPurchaseButton = multiplierPurchaseButton;
     }
     
     increaseDonutsAfterClick() {
-        this.donutCount++;
+        const newDonutCount = this.donutCount + this.multiplierCount;
+        this.donutCount = Math.round(newDonutCount * 100) / 100;
+        this.updateDonutDisplay();
     }
     
     updateDonutDisplay() {
         this.donutDisplay.innerText = this.donutCount;
     }
 
-    increaseAutoClickerCount() {
-        this.autoClickerCount++;
-    }
-    
-    updateAutoClicksDisplay() {
-        this.autoClicksDisplay.innerText = this.autoClickerCount;
-    }
-    
     subtractDonutsAfterClickerPurchase() {
         this.donutCount -= this.autoClickerCost;
+        this.updateDonutDisplay();
+    }
+    
+    increaseAutoClickerCount() {
+        this.autoClickerCount++;
+        this.updateAutoClickerCount();
+    }
+
+    updateAutoClickerCount() {
+        this.autoClicksDisplay.innerText = this.autoClickerCount;
     }
 
     increaseCostOfAutoClicker() {
-        this.autoClickerCost += this.autoClickerCost * .1;
+        const newAutoClickerCost = this.autoClickerCost * 1.1;
+        this.autoClickerCost = Math.round(newAutoClickerCost * 100) / 100;
+        this.updateCostOfAutoClicker();
+    }
+
+    updateCostOfAutoClicker() {
+        this.autoClickPurchaseButton.innerText = "PURCHASE CLICKS COST: " + this.autoClickerCost;
     }
 
     purchaseAutoClicker() {
-        this.autoClickerCost();
-        this.subtractDonutsAfterClickerPurchase();
-        this.increaseAutoClickerCount();
-        this.updateAutoClicksDisplay();
+        if (this.donutCount >= this.autoClickerCost) {
+            this.subtractDonutsAfterClickerPurchase();
+            this.increaseAutoClickerCount();
+            this.increaseCostOfAutoClicker();
+        } return;
+        // alert("You need some more donuts, doughboy!")
+    }
+
+    subtractDonutsAfterMultiplierPurchase() {
+        this.donutCount -= this.multiplierCost;
         this.updateDonutDisplay();
+    }
+    
+    increaseMultiplierCount() {
+        this.multiplierCount += .2;
+        this.updateMultiplierCount();
+    }
+
+    updateMultiplierCount() {
+        this.multiplierDisplay.innerText = this.multiplierCount + "x";
+    }
+
+    increaseCostOfMultiplier() {
+        const newMultiplierCost = this.multiplierCost * 1.1;
+        this.multiplierCost = Math.round(newMultiplierCost * 100) / 100;
+        this.updateCostOfMultiplier();
+    }
+
+    updateCostOfMultiplier() {
+        this.multiplierPurchaseButton.innerText = "PURCHASE MULTIPLIER COST: " + this.multiplierCost;
+    }
+
+    purchaseMultiplier() {
+        if (this.donutCount >= this.multiplierCost) {
+            this.subtractDonutsAfterMultiplierPurchase();
+            this.increaseMultiplierCount();
+            this.increaseCostOfMultiplier();
+        } return;
+        // alert("You need some more donuts, doughboy!")
+    }
+
+    reset() {
+        this.donutCount = 0;
+        this.autoClickerCount = 0;
+        this.multiplierCount = 1;
+        this.autoClickerCost = 100;
+        this.multiplierCost = 10;
+        this.updateDonutDisplay();
+        this.updateAutoClickerCount();
+        this.updateMultiplierCount();
+        this.updateCostOfAutoClicker();
+        this.updateCostOfMultiplier();
     }
 }
