4b15a532a0ca52092a6d799c4844c3693363b0a5
Sara Thornsberry <sarathornsberry@saras-mbp.home>
2020-07-02 21:40:50 -0400

    removed extra class, purchase button is working

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,19 @@
 const donutDisplay = document.querySelector(".click-handler__donut-display");
+const autoClicksDisplay = document.querySelector(".auto-clicker__auto-clicks-display");
 const donutMaker = new DonutMaker();
-const bakeButton = document.querySelector(".click-handler__bake-button");
 
+const bakeButton = document.querySelector(".click-handler__bake-button");
 bakeButton.addEventListener("click", () => {
     event.preventDefault();
     donutMaker.increaseDonutsAfterClick();
     donutMaker.updateDonutDisplay();
 })
 
-const autoClicksDisplay = document.querySelector(".auto-clicker__auto-clicks-display");
-const autoClicker = new AutoClicker();
 const purchaseButton = document.querySelector(".auto-clicker__purchase-button");
-
 purchaseButton.addEventListener("click", () => {
     event.preventDefault();
-    autoClicker.increaseAutoClickerCount();
-    autoClicker.subtractDonutsAfterClickerPurchase();
-    autoClicker.updateAutoClicksDisplay();
+    donutMaker.increaseAutoClickerCount();
+    donutMaker.subtractDonutsAfterClickerPurchase();
+    donutMaker.updateAutoClicksDisplay();
+    donutMaker.updateDonutDisplay();
 });
--- src/js/donutMaker.js
+++ src/js/donutMaker.js
@@ -3,6 +3,9 @@ class DonutMaker {
     constructor() {
         this.donutCount = 0;
         this.donutDisplay = donutDisplay;
+        this.autoClickerCount = 0;
+        this.autoClicksDisplay = autoClicksDisplay;
+        this.autoClickerCost = 100;
     }
     
     increaseDonutsAfterClick() {
@@ -13,4 +16,27 @@ class DonutMaker {
         this.donutDisplay.innerText = this.donutCount;
     }
 
+    increaseAutoClickerCount() {
+        this.autoClickerCount++;
+    }
+    
+    updateAutoClicksDisplay() {
+        this.autoClicksDisplay.innerText = this.autoClickerCount;
+    }
+    
+    subtractDonutsAfterClickerPurchase() {
+        this.donutCount -= this.autoClickerCost;
+    }
+
+    increaseCostOfAutoClicker() {
+        this.autoClickerCost += this.autoClickerCost * .1;
+    }
+
+    purchaseAutoClicker() {
+        this.autoClickerCost();
+        this.subtractDonutsAfterClickerPurchase();
+        this.increaseAutoClickerCount();
+        this.updateAutoClicksDisplay();
+        this.updateDonutDisplay();
+    }
 }
